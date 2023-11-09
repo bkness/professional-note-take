1e0ca13f42c4f38ccc41049a891de2eddad28b9c
bkness <kbrandon863@gmail.com>
2023-11-08 17:44:50 -0700

    worked diligently with a tutor and was able to finish the application in one hour with them minuse the delete features. added express and fs packages abd created db.json the get /notes returns the notes html get * returns the index.html the api/notes reads the db.json and the api/notes receives a new note to save on the request body and returns the new note to the client

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # professional-note-take
 Professional note taker for personal notes
+go to package json for install always want to be on the root 
---
+++ routes/notes.js
@@ -0,0 +1,50 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))); // reads all files from json file and then sends it to the client in json format (stringifies and does other fun stuff consult google laster pls)
+});
+
+// DELETE Route for a specific note
+tips.delete('/:note_id', (req, res) => {
+  const tipId = req.params.note_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => { // note hads a titlke and text 
+  console.log(req.body);
+
+  const { title, text } = req.body;  // title and text propertiers of req body
+
+  if (req.body) {
+    const newNotes = {
+      title, text,   // takes the title and text and makes a new id for our database (json file)
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNotes, './db/db.json');
+    res.json(`Notes added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
